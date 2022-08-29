831ef3fc3a104bc026bd7252a9d27e7b891a6447
Kevin Getz <kgetz32@gmail.com>
2022-08-29 10:01:17 -0700

    Created basic math  calc functions

---
+++ script.js
@@ -0,0 +1,53 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+// console.log(add(2,3));
+// console.log(subtract(2,3));
+// console.log(multiply(2,3));
+// console.log(divide(2,3));
+
+
+
+
+function operate(num1, num2, operator) {
+
+    return 
+
+}
+
+
+// let operator = prompt();
+// let addButton = add;
+// let subtractButton = subtract;
+// let multiplyButton = multiply;
+// let divideButton = divide;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
