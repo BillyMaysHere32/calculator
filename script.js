eec6349643a593ee3c23b92da57bad21fe1b6296
Kevin Getz <kgetz32@gmail.com>
2022-08-29 13:54:07 -0700

    added listener for number buttons to show on display

--- script.js
+++ script.js
@@ -22,11 +22,27 @@ function divide (num1, num2) {
 function operate(firstNum, secondNum, operator) {
     return operator(firstNum, secondNum)
 }
-// console.log(operate(2, 3, subtract));
+//console.log(operate(2.3, 3, subtract));
+
+
+const numButtons = document.querySelectorAll('#numButton');
+ // we use the .forEach method to iterate through each button
+ numButtons.forEach((button) => {
+// and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        displayNum(button.textContent);
+        });
+    });
+    function displayNum(number) {
+        currentDisplay.textContent += number
+      }
+const currentDisplay = document.getElementById('display')
 
 
 
-// let operator = prompt();
+
+
+
 // let addButton = add;
 // let subtractButton = subtract;
 // let multiplyButton = multiply;
