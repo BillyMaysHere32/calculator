91c7356684c867b160da5b331bf8fbf5bdfb79bd
Kevin Getz <kgetz32@gmail.com>
2022-08-30 11:44:02 -0700

    switched order of display divs so first number and operator appear on top of screen, also added reset function

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+let firstNum = "";
+let secondNum = "";
+let currentOp = null;
+let shouldResetScreen = false
+
+const currentDisplay = document.getElementById('initialDisplay')
+const endDisplay = document.getElementById('finalDisplay')
+const numButtons = document.querySelectorAll('#numButton');
+const opButtons = document.querySelectorAll('#opButton');
+
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        displayNum(button.textContent);
+        });
+    });
+    function displayNum(number) {
+        if (currentDisplay.textContent === '0' || shouldResetScreen)
+    resetScreen()
+    currentDisplay.textContent += number
+      }
+
+
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        displayOp(button.textContent);
+        });
+    });
+
+function displayOp(operator) {
+    if (currentOp !== null) 
+    calcNum();
+    firstNum = currentDisplay.textContent;
+    currentOp = operator;
+    endDisplay.textContent = `${firstNum} ${currentOp}`
+    shouldResetScreen = true;
+
+}
+
+function calcNum() {
+    secondNum = currentDisplay.textContent;
+    currentDisplay.textContent =
+    operate(firstNum, secondNum, currentOp);
+    endDisplay.textContent = `${firstNum} ${currentOp}`
+}
+
+
+
+function operate(firstNum, secondNum, operator) {
+    if (operator = "+") {
+        return add(firstNum, secondNum);
+    } else if (operator = "-") {
+        return subtract(firstNum, secondNum);
+    } else if (operator = "*") {
+        return multiply(firstNum, secondNum);
+    } else if (operator = "/") {
+        return divide(firstNum, secondNum);
+    }
+}
+// //console.log(operate(2.3, 3, subtract));
+
+// const clearButton = document.querySelectorAll('clearButton');
+// clearButton.addEventListener('click', clearScreen);
+// const clearScreen = () => {
+//     currentDisplay.textContent = ''
+//     finalDisplay.textContent = ''
+// }
+
+// let equalsButton = document.getElementById('equals')
+// equalsButton.addEventListener('click', () => {
+
+
+// }
+
+function resetScreen() {
+    currentDisplay.textContent = ''
+    shouldResetScreen = false
+}
+
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -14,70 +92,6 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
-// console.log(add(2,3));
-// console.log(subtract(2,3));
-// console.log(multiply(2,3));
-// console.log(divide(2,3));
-let firstNum = "";
-let secondNum = "";
-let currentOp ="";
-const currentDisplay = document.getElementById('initialDisplay')
-const endDisplay = document.getElementById('finalDisplay')
-
-const numButtons = document.querySelectorAll('#numButton');
- // we use the .forEach method to iterate through each button
- numButtons.forEach((button) => {
-// and for each one we add a 'click' listener
-    button.addEventListener('click', () => {
-        displayNum(button.textContent);
-        });
-    });
-    function displayNum(number) {
-        currentDisplay.textContent += number;
-      }
-
-const opButtons = document.querySelectorAll('#opButton');
-opButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        displayOp(button.textContent);
-        });
-    });
-    function displayOp(operator) {
-        calcNum();
-        firstNum = currentDisplay.textContent;
-        currentOp = operator;
-        endDisplay.textContent = `${firstNum} ${currentOp}`
-
-      }
-
-      function calcNum() {
-        secondNum = currentDisplay.textContent;
-        currentDisplay.textContent =
-        operate(firstNum, secondNum, currentOp);
-        endDisplay.textContent = `${firstNum} ${currentOp}`
-      }
-
-
-
-function operate(firstNum, secondNum, operator) {
-    if (operator = "+") {
-        return add(firstNum, secondNum);
-    } else if (operator = "-") {
-        return subtract(firstNum, secondNum);
-    } else if (operator = "*") {
-        return multiply(firstNum, secondNum);
-    } else if (operator = "/") {
-        return divide(firstNum, secondNum);
-    }
-}
-//console.log(operate(2.3, 3, subtract));
-
-const clearButton = document.querySelectorAll('clearButton');
-clearButton.addEventListener('click', clearScreen);
-const clearScreen = () => {
-    currentDisplay.textContent = ''
-    finalDisplay.textContent = ''
-}
 
 
 
